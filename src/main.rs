d71b69ac0db3e1b7028bd27c4bfe4a721f0a6c69
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-10-02 01:04:08 +0200

    added support for parsing command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+/// Arguments for this program
+#[derive(Parser)]
+#[command(
+    about = "A tool for converting JSON to C# classes",
+    long_about = "A tool for converting JSON to C# classes"
+)]
+struct Args {
+    /// Path of the file to parse
+    #[arg(short = 'f', long = "file")]
+    file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Args::parse();
+
+    println!("Sharpening '{}'", args.file);
 }
