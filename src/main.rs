aeed8fcc0be6c5115e908be6427c2d57c69ca399
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-10-02 02:53:35 +0200

    simple version without support for models / arrays

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use serde_json::{Map, Value};
 use std::fs;
 use std::path::Path;
 
@@ -16,20 +17,60 @@ struct Args {
 
 /// Contains the contents this parser can convert
 struct ClassContents {
-    name: String,
-    fields: Vec<String>,
+    contents: Value,
 }
 impl ClassContents {
     fn new(raw_json: &String) -> Self {
+        let parsed_value: Value = serde_json::from_str(raw_json).expect("Could not parse file");
+
         Self {
-            name: String::new(),
-            fields: Vec::new(),
+            contents: parsed_value,
         }
     }
     fn get_csharp_class(&self) -> String {
-        unimplemented!()
+        let contents = &self.contents;
+
+        // Loop through all contents - we need all fields
+        get_string_value_from_obj_map(&contents.as_object().expect("not an object"))
     }
 }
+
+fn get_string_value_from_obj_map(string_value: &Map<String, Value>) -> String {
+    let mut lines = String::new();
+
+    for (key, value) in string_value {
+        let line = format!(
+            "public {} {} = {}\n",
+            get_type_from_value(&value),
+            key,
+            get_value_from_type(&value)
+        );
+        lines.push_str(line.as_str());
+    }
+
+    lines
+}
+fn get_type_from_value(value: &Value) -> String {
+    match value {
+        Value::Null => String::from("?"),
+        Value::Bool(b) => String::from("bool"),
+        Value::Number(n) => String::from("number"),
+        Value::String(s) => String::from("string"),
+        Value::Array(a) => String::from("[]"),
+        Value::Object(o) => String::from("object"),
+    }
+}
+fn get_value_from_type(value: &Value) -> String {
+    match value {
+        Value::Null => String::from("NONE"),
+        Value::Bool(b) => b.to_string().to_lowercase(),
+        Value::Number(n) => n.to_string(),
+        Value::String(s) => format!("\"{}\"", s),
+        Value::Array(a) => String::from("[...]"),
+        Value::Object(o) => String::from("{...}"),
+    }
+}
+
 fn main() {
     let args: Args = Args::parse();
 
