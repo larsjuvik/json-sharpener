13aa891f029fd1abc0a6cd63aab1c30671b37d56
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-10-02 18:12:32 +0200

    added string test

--- corelib/src/tests.rs
+++ corelib/src/tests.rs
@@ -15,3 +15,17 @@ fn test_correct_bool_property() {
 
     assert_eq!(output, expected_output);
 }
+
+#[test]
+fn test_correct_string_property() {
+    let json = r#"{ "stringValue": "test" }"#;
+    let expected_output = r#"class TestClass
+{
+    public string StringValue { get; set; }
+}"#;
+
+    let parsed_data = ClassContents::new(&json.to_string(), class_name.to_string());
+    let output = parsed_data.get_csharp_output();
+
+    assert_eq!(output, expected_output);
+}
