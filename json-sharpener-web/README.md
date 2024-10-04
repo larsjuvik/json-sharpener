20f7d23a9ad6606b030bea9ba5882e70fa335ff0
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-10-04 23:57:04 +0200

    added Next.js project

---
+++ json-sharpener-web/README.md
@@ -0,0 +1,9 @@
+# JSON Sharpener Web
+
+## Getting Started
+
+Run the development server:
+
+```bash
+npm run dev
+```
---
+++ json-sharpener-web/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "JSON Sharpener",
+  description: "Convert JSON to C# classes",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={inter.className}>
+      <body>{children}</body>
+    </html>
+  );
+}
