54232b95295b0bd83fe6b504ed7349a316dd9985
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-10-05 20:01:17 +0200

    updated application to correcly load WASM library

--- json-sharpener-web/app/page.tsx
+++ json-sharpener-web/app/page.tsx
@@ -1,41 +1,57 @@
 "use client";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import Editor from "react-simple-code-editor";
 import Prism, { highlight } from "prismjs";
 import "prismjs/themes/prism.css";
 import "prismjs/components/prism-json";
 import "prismjs/components/prism-csharp";
-import init, { convert_json_to_csharp } from "@/lib/wasm/json_sharpener_wasm";
 
 export default function Home() {
-  const [libraryLoaded, setLibraryLoaded] = useState(false);
   const [inputText, setInputText] = useState("");
   const [outputText, setOutputText] = useState("");
 
-  async function loadLibrary() {
-    await init();
-    setLibraryLoaded(true);
-  }
-  loadLibrary();
+  const [convertJsonToCSharp, setConvertJsonToCSharp] = useState<
+    undefined | ((json: string) => string)
+  >();
 
-  function inputTextChanged(event: ChangeEvent<HTMLTextAreaElement>): void {
-    if (!libraryLoaded) return;
-    const newOutput = convert_json_to_csharp(event.target.value);
-    setOutputText(newOutput);
-  }
+  // Load WASM library
+  useEffect(() => {
+    const loadWasm = async (): Promise<
+      undefined | ((json: string) => string)
+    > => {
+      try {
+        const libraryModule = await import("@/public/wasm/json_sharpener_wasm");
+        const libUrl = "/wasm/json_sharpener_wasm_bg.wasm";
+        await libraryModule.default(libUrl);
+        return libraryModule.convert_json_to_csharp;
+      } catch (error) {
+        console.error("Failed to load WASM module", error);
+      }
+    };
+
+    loadWasm().then((r) => {
+      if (r !== undefined) {
+        setConvertJsonToCSharp(() => r);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (convertJsonToCSharp === undefined) return;
+    setOutputText(convertJsonToCSharp(inputText));
+  }, [inputText]);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1>JSON Sharpener</h1>
-        <h2>WASM Library Loaded: {libraryLoaded}</h2>
+        <h2>WASM Library Loaded</h2>
         <div className="flex flex-row gap-10">
           <Editor
+            className="bg-white w-52"
             value={inputText}
             onValueChange={(code) => setInputText(code)}
-            highlight={(code) =>
-              highlight(code, Prism.languages.csharp, "csharp")
-            }
+            highlight={(code) => highlight(code, Prism.languages.json, "json")}
             padding={10}
             style={{
               fontFamily: "monospace",
@@ -43,10 +59,11 @@ export default function Home() {
             }}
           />
           <Editor
+            className="bg-white w-52"
             value={outputText}
             onValueChange={(code) => setOutputText(code)}
             highlight={(code) =>
-              Prism.highlight(code, Prism.languages.json, "json")
+              Prism.highlight(code, Prism.languages.csharp, "csharp")
             }
             padding={10}
             style={{
