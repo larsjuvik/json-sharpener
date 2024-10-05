429cbdea846f2a0183c62780de8267ea27ef2ed5
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2024-10-05 02:03:10 +0200

    added support for using WASM in component

--- Dockerfile
+++ Dockerfile
@@ -16,11 +16,11 @@ FROM node:22-alpine AS base
 WORKDIR /app
 
 # Dependencies for Next.js app
-COPY ./json-sharpener-web/*.json ./
+COPY ./json-sharpener-web/*.json .
 RUN npm install
 
 COPY ./json-sharpener-web/ .
-COPY --from=rust-base /app/json-sharpener-wasm/pkg ./public/wasm
+COPY --from=rust-base /app/json-sharpener-wasm/pkg/* ./app/lib/wasm
 
 # Build
 ENV NEXT_TELEMETRY_DISABLED=1
--- json-sharpener-web/app/page.tsx
+++ json-sharpener-web/app/page.tsx
@@ -1,12 +1,38 @@
-import { useState } from "react";
+"use client";
+import init, { convert_json_to_csharp } from "@/lib/wasm/json_sharpener_wasm";
+import { ChangeEvent, useState } from "react";
 
 export default function Home() {
   const [libraryLoaded, setLibraryLoaded] = useState(false);
+  const [inputText, setInputText] = useState("");
+  const [outputText, setOutputText] = useState("");
+
+  async function loadLibrary() {
+    await init();
+    setLibraryLoaded(true);
+  }
+  loadLibrary();
+
+  function inputTextChanged(event: ChangeEvent<HTMLTextAreaElement>): void {
+    if (!libraryLoaded) return;
+    const newOutput = convert_json_to_csharp(event.target.value);
+    setOutputText(newOutput);
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1>JSON Sharpener</h1>
         <h2>WASM Library Loaded: {libraryLoaded}</h2>
+        <div className="flex flex-row gap-10">
+          <textarea
+            className="resize rounded border border-purple-600 bg-slate-700 text-blue-500"
+            onChange={inputTextChanged}></textarea>
+          <textarea
+            className="resize rounded border border-purple-600 bg-slate-700 text-blue-500"
+            value={outputText}
+            readOnly></textarea>
+        </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
